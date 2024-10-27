94e60b64657f4d1949cd5c2aa1a749485bacb242
Angel Henao <ay.henao@uniandes.edu.co>
2024-10-27 02:16:30 -0500

    Include editorial and book models, book-list component and prepare tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BookListComponent} from "./book/book-list/book-list.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, BookListComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +28,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('mynewapp');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mynewapp');
-  });
 });
