33468900f16de1502e9bcda2124c4307412a60da
Angel Henao <ay.henao@uniandes.edu.co>
2024-10-27 01:24:55 -0500

    Include component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Listar Cursos'`, () => {
+  it(`should have as title 'tutorialcourses'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Listar Cursos');
+    expect(app.title).toEqual('tutorialcourses');
   });
 
 });
